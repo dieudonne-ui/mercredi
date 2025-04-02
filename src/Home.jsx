8b9223e7d5c3aa750c90641b7e78065ec0de95a1
gabrielokeke <okekegabriel84@gmail.com>
2025-04-02 10:17:42 +0100

    Update Commit

---
+++ src/Home.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <>
+        <header>
+            <div className="logo">LOGO</div>
+            <div className="navbar">
+                <ul>
+
+                </ul>
+                
+                <button className="btn">Connecter/S'inscrire</button>
+            </div>
+        </header>
+        <footer>
+            <div className="logo">LOGO</div>
+            <div>
+                <ul>
+                <a href="#">Home</a>
+                <a href="#">Profile</a>
+                <a href="#">Form</a>
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                <a href="#">Home</a>
+                <a href="#">Profile</a>
+                <a href="#">Form</a>
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                <a href="#">Home</a>
+                <a href="#">Profile</a>
+                <a href="#">Form</a>
+                </ul>
+            </div>
+        </footer>
+        </>
+    </div>
+  )
+}
+
+export default Home
